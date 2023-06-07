f4020cd3d8f078553f095f42cf564576074ec7e4
Emilio Ormeño <eormneno@gmail.com>
2023-06-06 22:18:16 -0300

    d

---
+++ resources/lang/es2/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+return [ 
+
+    'failed' => 'Las credenciales introducidas son incorrectas.',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Demasiados intentos de acceso. Inténtelo de nuevo en :seconds segundos.',
+];
---
+++ resources/lang/es2/registration.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+   
+    'name' => 'Nombre',
+    'email' => 'Correo electrónico',
+    'password' => 'Contraseña',
+    'confirm_password' => 'Confirmar contraseña',
+    'role' => '¿Qué tipo de cuenta?',
+    'already_registered' => '¿Ya tienes una cuenta?',
+    'register' => 'Registrarse',
+    'select_role' => 'Selecciona el tipo de cuenta',
+    'role_recruiter' => 'Reclutador - Publica vacantes',
+    'role_teacher' => 'Docente - Aplica a vacantes',
+
+];
