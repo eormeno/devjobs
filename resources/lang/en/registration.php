7b36506ef636ed875cd4f0ac53c94d581b23911d
Emilio Ormeño <eormneno@gmail.com>
2023-06-06 22:27:19 -0300

    Added en locale

---
+++ resources/lang/en/registration.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'name' => 'Name',
+	'email' => 'Email',
+	'password' => 'Password',
+	'confirm_password' => 'Confirm Password',
+	'role' => 'Type of account',
+	'already_registered' => 'Already registered?',
+	'register' => 'Register',
+	'select_role' => 'Select a type of account',
+	'role_recruiter' => 'Recruiter - Publish vacancies',
+	'role_teacher' => 'Teacher - Apply for vacancies',
+];
