38b6b53e730b21303ec2cd491d2a033d8a4cc3e6
DAMO\Emilio <eormeno@gmail.com>
2023-06-23 12:37:28 -0300

    dddd

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,20 @@ use App\Http\Controllers\NotificationController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+	return view('welcome');
+})->name('home');
 
-Route::get('dashboard', [VacantController::class, 'index'])->middleware(['auth', 'verified'])->name('vacancies.index');
+Route::get('dashboard', [VacantController::class, 'index'])->middleware(['auth', 'verified', 'recruiter'])->name('vacancies.index');
 Route::get('vacancies/create', [VacantController::class, 'create'])->middleware(['auth', 'verified'])->name('vacancies.create');
 Route::get('vacancies/{vacant}/edit', [VacantController::class, 'edit'])->middleware(['auth', 'verified'])->name('vacancies.edit');
 Route::get('vacancies/{vacant}', [VacantController::class, 'show'])->name('vacancies.show');
-Route::get('notifications', NotificationController::class)->name('notifications');
+Route::get('notifications', NotificationController::class)->middleware(['auth', 'verified', 'recruiter'])->name('notifications');
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+	Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+	Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+	Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
